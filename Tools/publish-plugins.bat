0e14119d88caa80f9160d9aec54bdabdda69a47d
Lcnvdl <programando.soft@gmail.com>
2023-07-24 03:26:20 -0300

    Update publish-plugins.bat

--- Tools/publish-plugins.bat
+++ Tools/publish-plugins.bat
@@ -6,11 +6,11 @@ if "%1"=="linux" goto linux
 goto end
 
 :windows
-dotnet publish /p:DebugType=None /p:DebugSymbols=false /p:PublishSingleFile=false -c Release -r win-x64 --output ../../dist/plugins/HtmlRun.SQL.NHibernate/win-x64
+dotnet publish /p:DebugType=None /p:DebugSymbols=false /p:PublishSingleFile=false -c Release --no-self-contained -r win-x64 --output ../../dist/plugins/HtmlRun.SQL.NHibernate/win-x64
 goto end
 
 :linux
-dotnet publish /p:DebugType=None /p:DebugSymbols=false /p:PublishSingleFile=false -c Release -r linux-x64 --output ../../dist/plugins/HtmlRun.SQL.NHibernate/linux-x64
+dotnet publish /p:DebugType=None /p:DebugSymbols=false /p:PublishSingleFile=false -c Release --no-self-contained -r linux-x64 --output ../../dist/plugins/HtmlRun.SQL.NHibernate/linux-x64
 goto end
 
 :end
