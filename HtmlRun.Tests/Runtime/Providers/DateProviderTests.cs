64d1e2b9acde740f20d50910e311c39ab1b2e59a
Lucho <programando.soft@gmail.com>
2024-03-25 16:29:59 -0300

    Test fix

--- HtmlRun.Tests/Runtime/Providers/DateProviderTests.cs
+++ HtmlRun.Tests/Runtime/Providers/DateProviderTests.cs
@@ -19,13 +19,13 @@ public class DateProviderTests : BaseProviderTests
   public void DateProvider_Timestamp_ShouldWorkFine()
   {
     long result = Convert.ToInt64(this.CallJsInstruction(DateInstructionsSet.Timestamp)?.ToString());
-    Assert.Equal(1704078000000L, result);
+    Assert.Equal(1704067200000L, result);
   }
 
   [Fact]
   public void DateProvider_TimestampInSeconds_ShouldWorkFine()
   {
     long result = Convert.ToInt64(this.CallJsInstruction(DateInstructionsSet.TimestampInSeconds)?.ToString());
-    Assert.Equal(1704078000L, result);
+    Assert.Equal(1704067200L, result);
   }
 }
--- HtmlRun.Tests/Stubs/DateTimeProviderStub.cs
+++ HtmlRun.Tests/Stubs/DateTimeProviderStub.cs
@@ -4,5 +4,5 @@ namespace HtmlRun.Tests.Stubs;
 
 public class DateTimeProviderStub : IDateTimeProvider
 {
-  public DateTime UtcNow { get; set; } = new DateTime(2024, 01, 01);
+  public DateTime UtcNow { get; set; } = new DateTime(2024, 01, 01, 0, 0, 0, DateTimeKind.Utc);
 }
